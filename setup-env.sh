313c77e8092364bf90525b23f0c56dd1eff0f446
Eduardo Betanzos <ebetanzos@hotmail.es>
2020-02-09 20:26:16 -0600

    Add first scripts

---
+++ setup-env.sh
@@ -0,0 +1,135 @@
+#! /bin/bash
+#========================================================================
+# Script name    :setup-env.sh
+# Description    :This script is for setting up my dev environment in 
+#                 Debian-based distros
+#                 Docker instalation only work in Ubuntu or Debain
+# Author         :Eduardo Betanzos Morales
+# Email          :ebetanzos@hotmail.es
+#
+#
+# Copyright © 2020  Eduardo Betanzos Morales 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#========================================================================
+
+
+# Update system
+#---------------------------------------------------------------
+echo "Update system"
+echo "    Packages list"
+echo "    --------------------------------------------------------------"
+apt update
+echo "    Installed packages"
+echo "    --------------------------------------------------------------"
+apt upgrade -y
+
+
+# Tools and programs
+#---------------------------------------------------------------
+echo "Install tools and programs"
+echo "    Install curl"
+echo "    --------------------------------------------------------------"
+apt install curl -y
+echo "    Install nano"
+echo "    --------------------------------------------------------------"
+apt install nano -y
+echo "    Install gnome-tweaks"
+echo "    --------------------------------------------------------------"
+apt install gnome-tweaks -y
+echo "    Install dconf-editor"
+echo "    --------------------------------------------------------------"
+apt install dconf-editor -y
+echo "    Install vlc"
+echo "    --------------------------------------------------------------"
+apt install vlc -y
+echo "    Install ffmpeg"
+echo "    --------------------------------------------------------------"
+apt install ffmpeg -y
+echo "    Install qbittorrent"
+echo "    --------------------------------------------------------------"
+apt install qbittorrent -y
+echo "    Install youtube-dl"
+echo "    --------------------------------------------------------------"
+curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
+echo "    Install gnome-boxes"
+echo "    --------------------------------------------------------------"
+apt install gnome-boxes -y
+echo "    Install gimp"
+echo "    --------------------------------------------------------------"
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+sudo apt update
+sudo apt install gimp -y
+
+
+# Dev tools
+#---------------------------------------------------------------
+echo "Setting up dev environment"
+## Git
+echo "    Install git"
+echo "    --------------------------------------------------------------"
+apt install git -y
+echo "        Setting up git"
+echo "    --------------------------------------------------------------"
+git config --global user.name "Eduardo Betanzos"
+git config --global user.email "ebetanzos@hotmail.es"
+git config --global core.autocrlf input
+git config --global core.editor nano
+git config --global alias.st status
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.lone 'log --oneline'
+git config --global alias.lds 'log --pretty=format:"%C(yellow)%h\ %ad%Cred%d\ %Creset%s%Cblue\ [%cn]" --decorate --date=short'
+git config --global alias.ldr 'log --pretty=format:"%C(yellow)%h\ %ad%Cred%d\ %Creset%s%Cblue\ [%cn]" --decorate --date=relative'
+
+## Docker
+echo "    Install docker"
+echo "    --------------------------------------------------------------"
+sudo apt remove docker docker-engine docker.io containerd runc
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates software-properties-common
+
+if cat /etc/*release | grep ^NAME | grep Debian; then
+    echo "        Preparing docker instalation for Debian"
+    echo "--------------------------------------------------------------"
+    apt install -y gnupg2
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"	
+    echo "        END DEBIAN CONFIG ------------------------------------"
+elif cat /etc/*release | grep ^NAME | grep Ubuntu; then
+    echo "        Preparing docker instalation for Ubuntu"
+    echo "--------------------------------------------------------------"
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"	
+    echo "        END UBUNTU CONFIG ------------------------------------"
+fi
+
+sudo apt update
+sudo apt install -y docker-ce
+### Run my containers
+echo "        Docker containers"
+echo "    --------------------------------------------------------------"
+#### Postgres 9.6
+echo "            - postgres9.6"
+echo "    --------------------------------------------------------------"
+docker run --name postgres9.6 -e POSTGRES_PASSWORD=postgres -d -p 5432:5432 postgres:9.6-alpine
+
+## pgAdmin III
+echo "    Install pgadminIII"
+echo "    --------------------------------------------------------------"
+apt install -y pgadmin3 --no-install-recommends
+
+echo
+echo
+echo "-------------------------------------------------------------------"
+echo "SUCCESSFUL!!"
---
+++ setup-wsl.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+#========================================================================
+# Script name    :setup-env.sh
+# Description    :This script is for setting up my dev environment in 
+#                 Debian-based WSL distros
+# Author         :Eduardo Betanzos Morales
+# Email          :ebetanzos@hotmail.es
+#
+#
+# Copyright © 2020  Eduardo Betanzos Morales 
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#========================================================================
+
+# Update system packages
+#---------------------------------------------------------------
+echo "UPDATE SYSTEM"
+echo
+echo "Packages list"
+echo "--------------------------------------------------------------"
+apt update
+echo "Installed packages"
+echo "--------------------------------------------------------------"
+apt upgrade -y
+
+
+# General settings
+#---------------------------------------------------------------
+echo
+echo
+echo "GENERAL SETTINGS"
+echo
+echo "Make alias 'll' for 'ls -lha'"
+echo "--------------------------------------------------------------"
+echo " " >> /home/betanzos/.bashrc
+echo " " >> /home/betanzos/.bashrc
+echo "alias ll='ls -lha'" >> /home/betanzos/.bashrc
+
+
+# Packages
+#---------------------------------------------------------------
+echo
+echo
+echo "INSTALL PACKAGES"
+echo
+echo "Install binutils"
+echo "--------------------------------------------------------------"
+apt install binutils -y
+echo
+echo "Install neofetch"
+echo "--------------------------------------------------------------"
+apt install neofetch -y
+
+
+# Tools and programs
+#---------------------------------------------------------------
+echo
+echo
+echo "TOOLS AND PROGRAMS"
+echo
+echo "Install curl"
+echo "--------------------------------------------------------------"
+apt install curl -y
+echo
+echo "Install ffmpeg"
+echo "--------------------------------------------------------------"
+apt install ffmpeg -y
+echo
+echo "Install youtube-dl"
+echo "--------------------------------------------------------------"
+curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
+
+
+# Dev environment
+#---------------------------------------------------------------
+echo "DEV ENVIRONMENT"
+echo
+## Git
+echo "Install git"
+echo "--------------------------------------------------------------"
+apt install git -y
+echo
+echo "Setting up git"
+echo "--------------------------------------------------------------"
+git config --global user.name "Eduardo Betanzos"
+git config --global user.email "ebetanzos@hotmail.es"
+git config --global core.autocrlf input
+git config --global core.editor nano
+git config --global alias.st status
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.lone 'log --oneline'
+git config --global alias.lds 'log --pretty=format:"%C(yellow)%h\ %ad%Cred%d\ %Creset%s%Cblue\ [%cn]" --decorate --date=short'
+git config --global alias.ldr 'log --pretty=format:"%C(yellow)%h\ %ad%Cred%d\ %Creset%s%Cblue\ [%cn]" --decorate --date=relative'
+
+## Packages for run JavaFX applications
+echo
+echo "Install needed packages for run JavaFX applications"
+echo "---------------------------------------------------------------------"
+apt install -y libgtk-3-0 libglu1-mesa
+# According JavaFX official site (how to build javafx)
+#apt install -y libgtk-3-0 libgl1-mesa-glx libx11-6 x11proto-core-dev 
+
+## Environment variables
+echo
+echo "Setting up JAVA_HOME"
+echo "--------------------------------------------------------------"
+echo " " >> /home/betanzos/.bashrc
+echo "JAVA_HOME=/mnt/d/tools/java/adoptopenjdk-11.0.5+10_hotspot-linux" >> /home/betanzos/.bashrc
+echo
+echo "Setting up MAVEN_HOME"
+echo "--------------------------------------------------------------"
+echo "MAVEN_HOME=/mnt/d/tools/maven/3.6.3-linux" >> /home/betanzos/.bashrc
+echo
+echo "Add JAVA_HOME and MAVEN_HOME to the PATH"
+echo "--------------------------------------------------------------"
+echo "PATH=$JAVA_HOME/bin:$MAVEN_HOME/bin:$PATH" >> /home/betanzos/.bashrc
+echo
+echo "Setting up DISPLAY"
+echo "--------------------------------------------------------------"
+echo " "
+echo "DISPLAY=:0" >> /home/betanzos/.bashrc
+echo
+echo "Applaying changes in ~/.bashrc"
+echo "--------------------------------------------------------------"
+. /etc/bash.bashrc
+
+echo
+echo
+echo "-------------------------------------------------------------------"
+echo "SUCCESSFUL!!"
